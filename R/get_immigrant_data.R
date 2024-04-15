169eaf27ea4cc3b135dbf73f2e85a0807535e178
Kyle Walker <kyle@walker-data.com>
2024-04-15 11:18:14 -0500

    update to latest data

--- R/get_immigrant_data.R
+++ R/get_immigrant_data.R
@@ -1,7 +1,7 @@
 library(tidycensus)
 library(tidyverse)
 
-vars <- load_variables(2018, "acs5", cache = TRUE) %>%
+vars <- load_variables(2022, "acs5", cache = TRUE) %>%
   dplyr::filter(str_detect(name, "B05006"))
 
 # Grab data without geometry as we'll want to wrangle the geometry separately
@@ -14,21 +14,22 @@ immigrant_data <- map_dfr(state_codes, ~{
             europe = "B05006_002",
             east_asia = "B05006_048",
             south_asia = "B05006_056",
-            se_asia = "B05006_067",
-            sw_asia = "B05006_078",
-            east_africa = "B05006_092",
-            central_africa = "B05006_099",
-            north_africa = "B05006_101",
-            southern_africa = "B05006_106",
-            west_africa = "B05006_109",
-            oceania = "B05006_117",
-            mexico = "B05006_139",
-            caribbean = "B05006_125",
-            central_amer = "B05006_138",
-            south_amer = "B05006_148",
-            canada = "B05006_161"
+            se_asia = "B05006_068",
+            sw_asia = "B05006_079",
+            east_africa = "B05006_096",
+            central_africa = "B05006_105",
+            north_africa = "B05006_110",
+            southern_africa = "B05006_116",
+            west_africa = "B05006_119",
+            oceania = "B05006_130",
+            mexico = "B05006_160",
+            caribbean = "B05006_140",
+            central_amer = "B05006_154",
+            south_amer = "B05006_164",
+            canada = "B05006_177"
           ), 
-          output = "wide")
+          output = "wide",
+          year = 2022)
 })
 
 immigrant_grouped <- immigrant_data %>%
@@ -44,7 +45,7 @@ immigrant_grouped <- immigrant_data %>%
             mexico = mexicoE,
             cs_amer_caribbean = (central_amerE - mexicoE) + caribbeanE + south_amerE,
             canada = canadaE) %>%
-  mutate(across(where(is.numeric), ~as.integer(.x / 25)))
+  pivot_longer(cols = europe:canada, names_to = "group", values_to = "n")
   
 write_rds(immigrant_grouped, "data/immigrant_grouped.rds")
 
--- R/process_tracts.R
+++ R/process_tracts.R
@@ -1,51 +1,75 @@
 library(tigris)
-library(lehdr) # remotes::install_github('jamgreen/lehdr')
+library(tidycensus)
 library(tidyverse)
 library(sf)
 library(tictoc)
-library(mapboxapi) # remotes::install_github('walkerke/mapboxapi')
+library(mapboxapi) 
 options(tigris_use_cache = TRUE)
+sf_use_s2(FALSE)
 
 state_codes <- c(state.abb, "DC")
 names(state_codes) <- state_codes
 immigrant_groups <- read_rds("data/immigrant_grouped.rds")
   
 # Cache the blocks to prevent download errors
-# purrr::walk(state_codes, ~blocks(state = .x, year = 2016))
+# purrr::walk(state_codes, ~blocks(state = .x, year = 2020))
+
+# Create a CT crosswalk between 2020 blocks and the new 2022 IDs
+ct_blocks <- blocks("CT", year = 2020) %>%
+  st_point_on_surface() %>%
+  select(STATEFP20:GEOID20)
+
+ct_counties <- counties("CT", year = 2022) %>%
+  select(NEWCOUNTYFP = COUNTYFP)
+
+ct_blocks_joined <- ct_blocks %>%
+  st_join(ct_counties) %>%
+  transmute(GEOID22 = paste0(STATEFP20, NEWCOUNTYFP, 
+                             TRACTCE20, BLOCKCE20),
+            GEOID20 = GEOID20) %>%
+  st_drop_geometry()
+
+ct_blocks_to_use <- blocks("CT", year = 2020) %>%
+  select(GEOID20, POP20) %>%
+  left_join(ct_blocks_joined, by = "GEOID20") %>%
+  mutate(GEOID20 = GEOID22) %>%
+  select(GEOID20, POP20)
+  
 
 state_dots <- purrr::map(state_codes, ~{
   print(sprintf("Processing %s...", .x))
-  state_blocks <- blocks(.x, year = 2016)
-  state_lodes <- grab_lodes(state = .x, year = 2016, lodes_type = "rac",
-                         segment = "S000")
   
+  if (.x == "CT") {
+    state_blocks <- ct_blocks_to_use
+  } else {
+    state_blocks <- blocks(.x, year = 2020)
+  }
+
   tic()
-  state_with_rac <- state_blocks %>%
-    filter(GEOID10 %in% state_lodes$h_geocode) %>%
-    transmute(tract_id = str_sub(GEOID10, end = -5)) %>%
+  state_with_pop <- state_blocks %>%
+    filter(POP20 > 0) %>%
+    transmute(tract_id = str_sub(GEOID20, end = -5)) %>%
     group_by(tract_id) %>%
     summarize()
   toc()
   
-  state_groups <- immigrant_groups %>%
-    filter(str_sub(GEOID, end = 2) == tigris:::validate_state(.x))
-  
-  group_names <- names(immigrant_groups)[3:11]
-  
-  joined_tracts <- state_with_rac %>%
+
+  joined_tracts <- state_with_pop %>%
     left_join(immigrant_groups, by = c("tract_id" = "GEOID")) %>%
-    mutate_at(vars(europe:canada), ~if_else(is.na(.x), 0L, .x))
+    mutate(n = ifelse(is.na(n), 0, n))
   
-  all_dots <- map(group_names, function(name) {
-    print(glue::glue("Processing {name}..."))
-    suppressMessages(st_sample(joined_tracts, size = joined_tracts[[name]], 
-                               exact = TRUE)) %>%
-      st_sf() %>%
-      mutate(group = name) %>%
-      st_transform(4326) 
-  }) %>%
-    reduce(rbind) %>%
-    slice_sample(prop = 1)
+  # Check to see if any groups need to be removed
+  cleaned_tracts <- joined_tracts %>%
+    group_by(group) %>%
+    filter(max(n, na.rm = TRUE) >= 25) %>%
+    ungroup()
+  
+  all_dots <- as_dot_density(
+    input_data = cleaned_tracts, 
+    value = "n",
+    values_per_dot = 25,
+    group = "group"
+  )
   
   # Write state dataset to temp file to guard against unforeseen errors
   write_rds(all_dots, glue::glue("data/temp/{.x}_dots.rds"))
@@ -69,14 +93,14 @@ state_dots_combined <- state_dots %>%
   st_as_sf(crs = 4326)
 
 tippecanoe(state_dots_combined, 
-           output = "data/state_dots3.mbtiles", 
-           layer_name = "state_dots3",
+           output = "data/state_dots_2022_acs.mbtiles", 
+           layer_name = "state_dots_2022_acs",
            max_zoom = 12,
            min_zoom = 3,
            drop_rate = 2)
 
-upload_tiles("data/state_dots3.mbtiles", 
+upload_tiles("data/state_dots_2022_acs.mbtiles", 
              username = "kwalkertcu",
-             tileset_name = "state_dots3")
+             tileset_name = "state_dots_2022acs")
 
 
